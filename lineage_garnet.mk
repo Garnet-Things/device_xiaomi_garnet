920642d9217afcde5564415c5c07c630accdbfe4
Garnet-Things <garnetthings@gmail.com>
2025-09-05 10:35:41 +0530

    garnet: Inherit 64 bit only

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -13,12 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 PRODUCT_NAME := lineage_garnet
 PRODUCT_DEVICE := garnet
