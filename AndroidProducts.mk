c6fa80aa07dbd25f86690e9be32c16bc15b1c41c
Garnet-Things <garnetthings@gmail.com>
2025-09-13 03:26:17 +0530

    garnet: Add AndroidProducts makefile

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+GARNET_DEVICE_PATH := device/xiaomi/garnet
+
+COMMON_LUNCH_CHOICES := \
+	lineage_garnet-aosp_current-eng \
+	lineage_garnet-aosp_current-user \
+	lineage_garnet-aosp_current-userdebug
