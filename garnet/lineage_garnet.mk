7e625aaec7b359ef81f5bbdca4cc879bd68e705b
Garnet-Things <garnetthings@gmail.com>
2025-09-13 03:28:38 +0530

    garnet: Set model, brand, device, name, manufacturer

---
+++ garnet/lineage_garnet.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MODEL := garnet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := garnet
+PRODUCT_NAME := lineage_garnet
+PRODUCT_MANUFACTURER := xiaomi
