c54f50dd9f48c9f028cbcd361a438cc3f20a0add
Garnet-Things <garnetthings@gmail.com>
2025-09-13 04:14:56 +0530

    garnet: Inherit core 64 bit makefile * Inherit full base telephony makefile * Lineage common full phone makefile

--- lineage_common.mk
+++ lineage_common.mk
@@ -3,3 +3,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+#
+# All components inherited here go to system image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+#
+# Inherit from LineageOS
+#
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
