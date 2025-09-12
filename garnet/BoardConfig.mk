f3e20e14f2c82addbf7c8a58640a3ba28b350e3b
Garnet-Things <garnetthings@gmail.com>
2025-09-13 03:30:27 +0530

    garnet: Create comman and garnet BoardConfig makefile

---
+++ garnet/BoardConfig.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+-include vendor/xiaomi/garnet/BoardConfigVendor.mk
+include $(GARNET_DEVICE_PATH)/BoardConfigCommon.mk
