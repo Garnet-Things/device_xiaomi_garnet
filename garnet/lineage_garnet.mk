4390c36698401a17974ef5710a52ba8e486a5d93
Garnet-Things <garnetthings@gmail.com>
2025-09-15 04:04:34 +0530

    common: Create common lineage makefile

--- garnet/lineage_garnet.mk
+++ garnet/lineage_garnet.mk
@@ -4,5 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from lineage common
+$(call inherit-product, $(COMMON_DEVICE_PATH)/lineage_common.mk)
+
 PRODUCT_DEVICE := garnet
+PRODUCT_MODEL := 2312DRA50G
 PRODUCT_NAME := lineage_garnet
