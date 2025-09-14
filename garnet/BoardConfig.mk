e58f8d87800efc2df1e4869f28681242f56b299f
Garnet-Things <garnetthings@gmail.com>
2025-09-15 04:06:38 +0530

    garnet: Create garnet specific BoardConfig

---
+++ garnet/BoardConfig.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_CONFIG := gki_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/garnet
