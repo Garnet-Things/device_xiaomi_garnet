dc634010bc2ea7086a59dafbc142610365968223
Garnet-Things <garnetthings@gmail.com>
2025-09-16 02:48:44 +0530

    garnet: Create device makefile

--- garnet/lineage_garnet.mk
+++ garnet/lineage_garnet.mk
@@ -7,6 +7,9 @@
 # Inherit from lineage common
 $(call inherit-product, $(COMMON_DEVICE_PATH)/lineage_common.mk)
 
+# Inherit from garnet device
+$(call inherit-product, $(GARNET_DEVICE_PATH)/device.mk)
+
 PRODUCT_DEVICE := garnet
 PRODUCT_MODEL := 2312DRA50G
 PRODUCT_NAME := lineage_garnet
