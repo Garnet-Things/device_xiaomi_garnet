11672129ffb9998f7fab785d726c37b99442e86b
Garnet-Things <garnetthings@gmail.com>
2025-10-12 01:31:08 +0530

    common: Create AndroidProducts makefile

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_garnet.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_garnet-aosp_current-user \
+    lineage_garnet-aosp_current-userdebug \
+    lineage_garnet-aosp_current-eng
