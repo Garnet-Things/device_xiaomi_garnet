37f60b729df21265a7e833076fcef8f5acf5c890
Garnet-Things <garnetthings@gmail.com>
2025-10-12 01:40:22 +0530

    Create lineage makefiles

---
+++ garnet/lineage_garnet.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common
+$(call inherit-product, $(COMMON_PATH)/lineage_common.mk)
+
+PRODUCT_DEVICE := garnet
+PRODUCT_MODEL := 2312DRA50G
+PRODUCT_NAME := lineage_garnet
---
+++ lineage_common.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Redmi
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="missi-user 15 AQ3A.240912.001 OS2.0.203.0.VNRINXM release-keys"
+
+BUILD_FINGERPRINT := qti/missi/missi:15/AQ3A.240912.001/OS2.0.203.0.VNRINXM:user/release-keys
